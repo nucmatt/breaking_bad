5c44faca386e53c32df8b79e8e66ed9232564a5c
Matthew Wessel <nucmatt@gmail.com>
2020-07-30 12:19:57 -0500

    Added Spinner component. This completes Traversy's video.

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 import CharacterItem from './CharacterItem';
-// Once you have your global state in App.js set as props passed in to your compoenent(in App.js), the props need to be passed down in to the component itself. This is done within the parentheses. The object in to parentheses here is simply destructuring so you don't have to use props.xxx all the time. 
+import Spinner from '../ui/Spinner';
+
+// Once you have your global state in App.js set as props passed in to your compoenent(in App.js), the props need to be passed down in to the component itself. This is done within the parentheses. The object in to parentheses here is simply destructuring so you don't have to use props.xxx all the time.
 const CharacterGrid = ({ items, isLoading }) => {
-    return isLoading ? (<h1>Loading...</h1>) : (<section className='cards'>
-        {/* Since the map is outputting JSX you want to use parentheses instead of curly braces as you would with normal JS. */}
-        {items.map(item => (
-            // <h1 key={item.char_id}>{item.name}</h1>
-            <CharacterItem key={item.char_id} item={item} />
-        ))}
-    </section>)
-}
+	return isLoading ? (
+		<Spinner />
+	) : (
+		<section className='cards'>
+			{/* Since the map is outputting JSX you want to use parentheses instead of curly braces as you would with normal JS. */}
+			{items.map((item) => (
+				// <h1 key={item.char_id}>{item.name}</h1>
+				<CharacterItem key={item.char_id} item={item} />
+			))}
+		</section>
+	);
+};
 
-export default CharacterGrid
-
+export default CharacterGrid;
