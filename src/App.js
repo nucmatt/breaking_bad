b496830f630a90b507fcd96a926dae1e23fd56c6
Matthew Wessel <nucmatt@gmail.com>
2020-07-30 10:27:03 -0500

    Created components folder and Header.js component. Imported Header.js to App.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/ui/Header.js';
 import './App.css';
 
 const App = () => {
-	return <div className='container'>Hello!</div>;
+	return <div className='container'>
+    <Header />
+  </div>;
 };
 
 export default App;
